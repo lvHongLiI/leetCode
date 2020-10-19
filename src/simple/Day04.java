133c800a91c099691160a38e3895167d1dc67fef
lvhonglii <1556282924@qq.com>
2020-10-19 21:49:12 +0800

    第5次提交

--- src/simple/Day04.java
+++ src/simple/Day04.java
@@ -74,18 +74,134 @@ public class Day04 {
     }
 
 
+    /**
+     * 762. 二进制表示中质数个计算置位
+     * @param L
+     * @param R
+     * @return
+     */
+    public int countPrimeSetBits(int L, int R) {
+        int sum=0;
+        for (int i = L; i <=R; i++) {
+            if (binaryCount(i)){
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+
+    private boolean binaryCount(int i) {
+        int count=0;
+        while (i>0){
+            count+=i&1;
+            i=i>>1;
+        }
+        if (count>1){
+            int sqrt = (int) Math.sqrt(count);
+            for (int j =2; j <=sqrt; j++) {
+                if (count%j==0)
+                    return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 771. 宝石与石头
+     * @param J
+     * @param S
+     * @return
+     */
+    public int numJewelsInStones(String J, String S) {
+        Map<Character,Integer> map=new HashMap<>();
+        char[] chars = S.toCharArray();
+        for (char c : chars) {
+            Integer integer = map.get(c);
+            if (integer==null){
+                map.put(c,1);
+            }else {
+                map.put(c,integer+1);
+            }
+        }
+        int j=0;
+        for (char c : J.toCharArray()) {
+            Integer integer = map.get(c);
+            if (integer!=null)
+                j+=integer;
+        }
+        return j;
+    }
+
+    /**
+     * 788. 旋转数字
+     * @param N
+     * @return
+     */
+    public int rotatedDigits(int N) {
+        int sum=0;
+        for (int i = 1; i <= N; i++) {
+            try {
+                if (goodNumbers(i)!=i)
+                    sum++;
+            }catch (Exception e){
+
+            }
+        }
+        return sum;
+    }
+    private int goodNumbers(int number) {
+
+            int num=number%10;
+            if (num==2){
+                num=5;
+            }else if (num==5){
+                num=2;
+            }else if (num==6){
+                num=9;
+            }else if (num==9){
+                num=6;
+            }else if (num==3||num==4||num==7){
+                throw new RuntimeException();
+            }
+            if (number>=10)
+             return goodNumbers(number/10)*10+num;
+            return num;
+
+
+    }
+
+    /**
+     * 796. 旋转字符串
+     * @param A
+     * @param B
+     * @return
+     */
+    public boolean rotateString(String A, String B) {
+        if (A==null||B==null)
+            return false;
+        if (A.length()!=B.length())
+            return false;
+        if (!A.equals(B)){
+            char[] a = A.toCharArray();
+            char[] b = B.toCharArray();
+            a:for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b.length; j++) {
+                    if (a[(i+j)%a.length]!=b[j])
+                        continue a;
+                }
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    
 
     public static void main(String[] args) {
-    TreeNode node=new TreeNode(3);
-    TreeNode nodel=new TreeNode(9);
-    TreeNode noder=new TreeNode(20);
-    TreeNode noderl=new TreeNode(15);
-    TreeNode noderr=new TreeNode(7);
-    noder.right=noderr;
-    noder.left=noderl;
-    node.left=nodel;
-    node.right=noder;
-        boolean balanced = new Day04().isBalanced(node);
+
+        boolean balanced = new Day04().rotateString("abcde","abced");
         System.out.println(balanced);
     }
 }
