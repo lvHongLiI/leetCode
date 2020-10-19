8dfb0f70ae0b91580ee96bf6de3e76764f0cfdf5
lvhonglii <1556282924@qq.com>
2020-10-19 20:35:47 +0800

    第4次提交

--- src/secondary/Day01.java
+++ src/secondary/Day01.java
@@ -1,5 +1,7 @@
 package secondary;
 
+import simple.ListNode;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,15 +33,51 @@ public class Day01 {
 
 
     /**
-     * 137. 只出现一次的数字 II
-     * @param nums
+     * 2. 两数相加
+     * @param l1
+     * @param l2
      * @return
      */
-    public int singleNumber(int[] nums) {
-        return 0;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1==null)
+            return l2;
+        if (l2==null)
+            return l1;
+        return addTwoNumbers(l1,l2,0);
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2,int on) {
+        if (l1==null&&l2==null){
+            if (on!=0)
+                return new ListNode(on);
+            return null;
+        }
+
+        int now=0;
+        ListNode lastl1=null;
+        ListNode lastl2=null;
+        if (l1==null){
+            now+=l2.val;
+            lastl2=l2.next;
+        }
+        else if (l2==null){
+            now+=l1.val;
+            lastl1=l1.next;
+        }else {
+            now+=l1.val+l2.val;
+            lastl2=l2.next;
+            lastl1=l1.next;
+        }
+        ListNode node = new ListNode((now + on)%10);
+        node.next= addTwoNumbers(lastl1, lastl2, (now + on)/10);
+        return node;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(new Day01().majorityElement(new int[]{1, 1, 1, 3, 3, 2, 2, 2}));
+        ListNode node1 = new ListNode(9, new ListNode(9, new ListNode(9)));
+        ListNode node2 = new ListNode(9);
+        ListNode node = new Day01().addTwoNumbers(node1, node2);
+        System.out.println(node);
     }
 }
