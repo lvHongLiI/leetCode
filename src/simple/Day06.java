a86ea0d5c4b216e9a70edbd6b8a748f45e6620bb
luhongli <luhongli123>
2020-12-04 16:55:48 +0800

    第12次提交

--- src/simple/Day06.java
+++ src/simple/Day06.java
@@ -1,11 +1,7 @@
 package simple;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import java.util.Collection;
-import java.util.HashMap;
+import java.math.BigDecimal;
+import java.util.*;
 
 
 /**
@@ -251,7 +247,492 @@ public class Day06 {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Day06().countCharacters(new String[]{"cat","bt","hat","tree"},"atach"));
+
+    /**
+     * 1470. 重新排列数组
+     * @param nums
+     * @param n
+     * @return
+     */
+    public int[] shuffle(int[] nums, int n) {
+        int[] arr=nums.clone();
+        for (int i = 0; i < nums.length; i++) {
+            if (i<n){
+                nums[i*2]=arr[i];
+            }else {
+                nums[(i%n)*2+1]=arr[i];
+            }
+        }
+        return nums;
     }
+
+
+    /**
+     * 1475. 商品折扣后的最终价格
+     * @param prices
+     * @return
+     */
+    public int[] finalPrices(int[] prices) {
+        int[] arr=new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            arr[i]=prices[i]-prices(prices,i);
+        }
+        return arr;
+    }
+    private int prices(int[] prices,int priceIndex){
+        for (int i = priceIndex+1; i < prices.length; i++) {
+            if (prices[i]<=prices[priceIndex])
+                return prices[i];
+        }
+        return 0;
+    }
+
+
+    /**
+     * 1480. 一维数组的动态和
+     * @param nums
+     * @return
+     */
+    public int[] runningSum(int[] nums) {
+        int[] arr=new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i]=nums[i];
+            if (i!=0){
+                arr[i]+=arr[i-1];
+            }
+        }
+        return arr;
+    }
+
+
+    /**
+     * 1486. 数组异或操作
+     * @param n
+     * @param start
+     * @return
+     */
+    public int xorOperation(int n, int start) {
+        int sum=0;
+        for (int i = 0; i < n; i++) {
+            sum^=start+i*2;
+        }
+        return sum;
+    }
+
+    /**
+     * 1491. 去掉最低工资和最高工资后的工资平均值
+     * @param salary
+     * @return
+     */
+    public double average(int[] salary) {
+        int max=salary[0];
+        int min=salary[salary.length-1];
+        int sum=0;
+        for (int sa : salary) {
+            if (max<sa){
+                max=sa;
+            }
+            if (min>sa){
+                min=sa;
+            }
+            sum+=sa;
+        }
+        return (sum-max-min)/((salary.length-2)*1.0);
+    }
+
+
+    /**
+     * 1502. 判断能否形成等差数列
+     * @param arr
+     * @return
+     */
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int x=arr[1]-arr[0];
+        for (int i = 2; i < arr.length; i++) {
+                if(arr[i]-arr[i-1]!=x){
+                    return false;
+                }
+        }
+        return true;
+    }
+
+
+    /**
+     * 1507. 转变日期格式
+     * @param date
+     * @return
+     */
+    public String reformatDate(String date) {
+        StringBuilder sb=new StringBuilder();
+        String[] month={"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        String[] split = date.split("\\s+");
+        sb.append(split[2]).append("-");
+        switch (split[1]){
+            case "Jan":
+                sb.append("01-");
+                break;
+            case "Feb":
+                sb.append("02-");
+                break;
+            case "Mar":
+                sb.append("03-");
+                break;
+            case "Apr":
+                sb.append("04-");
+                break;
+            case "May":
+                sb.append("05-");
+                break;
+            case "Jun":
+                sb.append("06-");
+                break;
+            case "Jul":
+                sb.append("07-");
+                break;
+            case "Aug":
+                sb.append("08-");
+                break;
+            case "Sep":
+                sb.append("09-");
+                break;
+            case "Oct":
+                sb.append("10-");
+                break;
+            case "Nov":
+                sb.append("11-");
+                break;
+            case "Dec":
+                sb.append("12-");
+                break;
+        }
+        if (split[0].length()==4){
+         sb.append(split[0].substring(0,2));
+        }else {
+            sb.append(0).append(split[0].substring(0,1));
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * 1512. 好数对的数目
+     * @param nums
+     * @return
+     */
+    public int numIdenticalPairs(int[] nums) {
+        int sum=0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int last=nums[i];
+            for (int j = i+1; j < nums.length; j++) {
+                if (last==nums[j]){
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 1518. 换酒问题
+     * @param numBottles
+     * @param numExchange
+     * @return
+     */
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum=numBottles;
+        while (numBottles>0){
+            sum+=numBottles/numExchange;
+            int n=0;
+            if (numBottles>numExchange){
+                n=  numBottles%numExchange;
+            }
+            numBottles=numBottles/numExchange+n;
+        }
+        return sum;
+    }
+
+
+    /**
+     * 1523. 在区间范围内统计奇数数目
+     * @param low
+     * @param high
+     * @return
+     */
+    public int countOdds(int low, int high) {
+        int num=(high-low)/2;
+        if (low%2==1||high%2==1){
+            num+=1;
+        }
+        return num;
+    }
+
+    /**
+     * 1528. 重新排列字符串
+     * @param s
+     * @param indices
+     * @return
+     */
+    public String restoreString(String s, int[] indices) {
+        char[]  chars=new char[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            chars[indices[i]]=s.charAt(i);
+        }
+
+        return new String(chars);
+    }
+
+    /**
+     * 1539. 第 k 个缺失的正整数
+     * @param arr
+     * @param k
+     * @return
+     */
+    public int findKthPositive(int[] arr, int k) {
+        int sum=0;
+        int[] nums=new int[1001];
+        for (int a : arr) {
+            nums[a]=1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]==0)
+                sum++;
+            if (sum==k)
+                return i;
+        }
+        return 1000+k-sum;
+    }
+
+    /**
+     * 1544. 整理字符串
+     * @param s
+     * @return
+     */
+    public String makeGood(String s) {
+        StringBuilder builder=new StringBuilder(s);
+        for (int i = 0; i < builder.length()-1;) {
+            if (Math.abs(builder.charAt(i)-builder.charAt(i+1))==32){
+                builder.deleteCharAt(i);
+                builder.deleteCharAt(i);
+                i=0;
+            }else {
+                i++;
+            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 1550. 存在连续三个奇数的数组
+     * @param arr
+     * @return
+     */
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int low=0;
+        int fast=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]%2==1){
+                fast++;
+            }else {
+                low=fast;
+            }
+            if (fast-low==3)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * 1556. 千位分隔数
+     * @param n
+     * @return
+     */
+    public String thousandSeparator(int n) {
+        StringBuilder sb=new StringBuilder();
+        int sum=0;
+        while (n>0){
+            if (sum==3){
+                sb.append(".");
+                sum=0;
+            }
+            sum++;
+            sb.append(n%10);
+            n/=10;
+        }
+        return sb.length()==0?"0":sb.reverse().toString();
+    }
+
+
+    /**
+     * 1576. 替换所有的问号
+     * @param s
+     * @return
+     */
+    public String modifyString(String s) {
+        StringBuilder builder=new StringBuilder(s);
+        for (int i = 0; i < builder.length(); i++) {
+            if (builder.charAt(i)=='?'){
+                    if (i==0){
+                        if (i==builder.length()-1){
+                            builder.deleteCharAt(i);
+                            builder.append('a');
+                        }else {
+                            char aChar = getChar(builder.charAt(i + 1));
+                            builder.deleteCharAt(i);
+                            builder.insert(i,aChar);
+                        }
+                    }else {
+                        if (i==builder.length()-1){
+                            char aChar = getChar(builder.charAt(i - 1));
+                            builder.deleteCharAt(i);
+                            builder.insert(i,aChar);
+                        }else {
+                            char aChar = getChar(builder.charAt(i - 1), builder.charAt(i + 1));
+                            builder.deleteCharAt(i);
+                            builder.insert(i,aChar);
+                        }
+                    }
+            }
+        }
+        return builder.toString();
+    }
+
+
+    private static char getChar(char... chars){
+        a:for (int i = 0; i < 26; i++) {
+            for (char aChar : chars) {
+                if (aChar-'a'==i){
+                    continue a;
+                }
+            }
+            return (char) (i+'a');
+        }
+        return 'a';
+    }
+
+    /**
+     * 1588. 所有奇数长度子数组的和
+     * @param arr
+     * @return
+     */
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum=0;
+        SegmentTree tree = new SegmentTree(arr);
+        for (int i = 1; i <=arr.length; i+=2) {
+            for (int j = 0; j <=arr.length-i; j++) {
+                sum+=tree.query(j,j+i-1);
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * 1592. 重新排列单词间的空格
+     * @param text
+     * @return
+     */
+    public String reorderSpaces(String text) {
+        int sum=0;
+        for (char c : text.toCharArray()) {
+            if (c==' '){
+                sum++;
+            }
+        }
+        if (sum==0)
+            return text;
+        String[] split = text.trim().split("\\s+");
+        int avg=0;
+        if (split.length>1){
+             avg=sum/(split.length-1);
+        }
+        StringBuilder builder=new StringBuilder();
+        for (int i = 0; i < split.length; i++) {
+            builder.append(split[i]);
+            if (i!=split.length-1){
+                for (int j = 0; j <avg ; j++) {
+                    builder.append(' ');
+                }
+                sum-=avg;
+            }
+
+        }
+
+        for (int i = 0; i < sum; i++) {
+            builder.append(' ');
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 1598. 文件夹操作日志搜集器
+     * @param logs
+     * @return
+     */
+    public int minOperations(String[] logs) {
+    int sum=0;
+        for (String log : logs) {
+            if (log.equals("../")){
+                if (sum>1){
+                    sum--;
+                }
+            }else if (log.equals("./")){
+
+            }else {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+
+    /**
+     * 1603. 设计停车系统
+     */
+    class ParkingSystem {
+        private int[] arr=new int[3];
+
+        public ParkingSystem(int big, int medium, int small) {
+            this.arr[0] = big;
+            this.arr[1] = medium;
+            this.arr[2]  = small;
+        }
+
+        public boolean addCar(int carType) {
+            if (arr[carType]>0){
+                arr[carType]--;
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    /**
+     * 1614. 括号的最大嵌套深度
+     * @param s
+     * @return
+     */
+    public int maxDepth(String s) {
+        int max=0;
+        int sum=0;
+        for (char c : s.toCharArray()) {
+            if (c=='('){
+                sum++;
+                if (max<sum){
+                    max=sum;
+                }
+            }else if (c==')'){
+                sum--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(new Day06().maxDepth("()(1)+((2))+(((3)))"));
+
+    }
+
 }
---
+++ src/simple/SegmentTree.java
@@ -0,0 +1,109 @@
+package simple;
+
+
+/**
+ * 线段树
+ */
+public class SegmentTree {
+
+    private int[] data;
+
+    private int[] tree;
+
+    //private Merge merge;
+    public SegmentTree(int[] arr) {
+        //this.merge=merge;
+        data=arr;
+        tree=  new int[data.length*4];
+        initialize(0,0,data.length-1);
+    }
+
+    private void initialize(int treeIndex,int l,int r){
+        if (l==r){
+            tree[treeIndex]=data[l];
+            return;
+        }
+
+        int mid=l+(r-l)/2;
+        int leftChild = leftChild(treeIndex);
+        int rightChild = rightChild(treeIndex);
+        initialize(leftChild,l,mid);
+        initialize(rightChild,mid+1,r);
+        tree[treeIndex]=tree[leftChild]+tree[rightChild];
+    }
+
+    public int query(int l,int r){
+        if (l<0||l>=data.length||r<0||r>=data.length||l>r)
+            throw new RuntimeException("传进来的索引不对");
+        return query(0,0,data.length-1,l,r);
+    }
+
+    private int query(int treeIndex,int l,int r,int queryL,int queryR){
+        if (l==queryL&&r==queryR){
+            return tree[treeIndex];
+        }
+        int mid=l+(r-l)/2;
+        int leftChild = leftChild(treeIndex);
+        int rightChild = rightChild(treeIndex);
+        if (queryL>mid)
+            return query(rightChild,mid+1,r,queryL,queryR);
+        if (queryR<=mid)
+            return query(leftChild,l,mid,queryL,queryR);
+        else {
+            int dataR = query(rightChild, mid + 1, r, mid+1, queryR);
+            int dataL = query(leftChild, l, mid, queryL, mid);
+            return dataL+dataR;
+        }
+    }
+
+    public void update(int index,int e){
+        if (index<0||index>data.length-1)
+            throw new  RuntimeException("传进来的索引不对");
+        data[index]=e;
+        update(0,0,data.length-1,index,e);
+
+    }
+
+    private void update(int treeIndex,int l,int r,int index, int e){
+        if (l==r&&l==index){
+            tree[treeIndex]=e;
+            return;
+        }
+        int mid=l+(r-l)/2;
+        int leftChild = leftChild(treeIndex);
+        int rightChild = rightChild(treeIndex);
+        if (index>mid){
+            update(rightChild,mid+1,r,index,e);
+        }else {
+            update(leftChild,l,mid,index,e);
+        }
+        tree[treeIndex]=tree[leftChild]+tree[rightChild];
+    }
+
+    private int leftChild(int index){
+        return index*2+1;
+    }
+
+    private int rightChild(int index){
+        return index*2+2;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder builder=new StringBuilder();
+        toString(0,0,data.length-1,builder);
+        return builder.toString();
+    }
+
+    private void toString(int index,int l,int r,StringBuilder builder){
+            if (l==r){
+                builder.append(tree[index]).append(",");
+                return;
+            }
+        int mid=l+(r-l)/2;
+        int leftChild = leftChild(index);
+        int rightChild = rightChild(index);
+        toString(leftChild,l,mid,builder);
+        toString(rightChild,mid+1,r,builder);
+    }
+}
